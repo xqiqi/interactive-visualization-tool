8c6ab88b2f39079c3c6c8d08ef0408b4945604f8
lwq <lwqiqi@foxmail.com>
2016-09-19 16:47:53 +0800

    add DBSCAN clustering method

--- RScripts/dataClusterProcess.R
+++ RScripts/dataClusterProcess.R
@@ -19,3 +19,24 @@ getKmeans <- function (o) {
     d$cluster <- kc$cluster
     return(toJSON(d))
 }
+
+getDbscan <- function (o) {
+    o <- fromJSON(o)
+
+    eps <- as.numeric(o$param)[1]
+    minpts <- as.numeric(o$param)[2]
+    dim <- as.numeric(o$dim)[1]
+    path <- "~/interactive-visualization-tool/uploads/tmp.csv"
+
+    d <- read.table(path, header=FALSE, sep=",", encoding="UTF-8")
+
+    if (dim == 2) {
+        data <- d[1:2]
+    } else if (dim ==3) {
+        data <- d[1:3]
+    }
+
+    kc <- dbscan(data, eps, minpts)
+    d$cluster <- kc$cluster
+    return(toJSON(d))
+}
